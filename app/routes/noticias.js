bb06a334d3b15690779dc2c0823a341da4b1d81a
angelilton Epifanio <epifanio.rds@gmail.com>
2017-11-02 16:21:55 -0300

    install do plugin do mysql - cod a busca no banco devolvendo um JSON

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,17 @@
 module.exports = (app) => {
   app.get('/noticias', (req, resp) => {
-    resp.render('noticias/noticias');
+    const mysql = require('mysql');
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'ykc75n7',
+      database: 'portal_noticias'
+    });
+
+    connection.query('select * from noticias', (error, result) => {
+      resp.send(result);
+    });
+    // resp.render('noticias/noticias');
   });
-}
+};
