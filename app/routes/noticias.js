04e2f4f97d82e1f79e2e55429c2d8fe364dd039a
angelilton Epifanio <epifanio.rds@gmail.com>
2017-11-03 01:16:01 -0300

    Modularização do acesso ao banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,7 @@
+const dbConnect = require('../../config/dbConnect');
 module.exports = (app) => {
+  let connection = dbConnect();
   app.get('/noticias', (req, resp) => {
-    const mysql = require('mysql');
-
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'ykc75n7',
-      database: 'portal_noticias'
-    });
 
     connection.query('select * from noticias', (error, result) => {
       resp.render('noticias/noticias', {noticias : result});
